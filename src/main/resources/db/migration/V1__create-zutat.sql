a53204da97df6f2b3db532541ea32a24a71932c1
Learwin <6223515+Learwin@users.noreply.github.com>
2024-12-04 19:53:03 +0100

    Proper Decimal values for Table Zutat

--- src/main/resources/db/migration/V1__create-zutat.sql
+++ src/main/resources/db/migration/V1__create-zutat.sql
@@ -1,14 +1,14 @@
 CREATE TABLE Zutat (
     ID                          BIGINT          NOT NULL AUTO_INCREMENT,
     NAME                        VARCHAR(255)    NOT NULL,
-    KCAL                        DECIMAL(5,4)         NULL,
-    FETT                        DECIMAL(5,4)          NULL,
-    GESAETTIGTE_FETTSAEUREN     DECIMAL(5,4)          NULL,
-    KOHLENHYDRATE               DECIMAL(5,4)          NULL,
-    ZUCKER                      DECIMAL(5,4)          NULL,
-    BALLASTSTOFFE               DECIMAL(5,4)          NULL,
-    EIWEISS                     DECIMAL(5,4)          NULL,
-    SALZ                        DECIMAL(5,4)          NULL,
+    KCAL                        DECIMAL(10,4)         NULL,
+    FETT                        DECIMAL(10,4)          NULL,
+    GESAETTIGTE_FETTSAEUREN     DECIMAL(10,4)          NULL,
+    KOHLENHYDRATE               DECIMAL(10,4)          NULL,
+    ZUCKER                      DECIMAL(10,4)          NULL,
+    BALLASTSTOFFE               DECIMAL(10,4)          NULL,
+    EIWEISS                     DECIMAL(10,4)          NULL,
+    SALZ                        DECIMAL(10,4)          NULL,
     FOTO                        VARCHAR(255)    NULL,
 
     CONSTRAINT pk_id PRIMARY KEY (ID)
