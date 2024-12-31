1637f5447546fc3d0c456f2059bc84c505f6370c
Learwin <6223515+Learwin@users.noreply.github.com>
2024-12-31 12:05:54 +0100

    Added missing CRUD operations to the entity

--- src/main/java/com/github/learwin/platepalbackend/controller/ZutatDerWocheController.java
+++ src/main/java/com/github/learwin/platepalbackend/controller/ZutatDerWocheController.java
@@ -1,11 +1,14 @@
 package com.github.learwin.platepalbackend.controller;
 
+import com.github.learwin.platepalbackend.entity.Zutat;
 import com.github.learwin.platepalbackend.entity.ZutatDerWoche;
 import com.github.learwin.platepalbackend.repository.ZutatDerWocheRepository;
-import io.micronaut.http.annotation.Controller;
-import io.micronaut.http.annotation.Get;
+import io.micronaut.http.HttpResponse;
+import io.micronaut.http.HttpStatus;
+import io.micronaut.http.annotation.*;
 import io.micronaut.scheduling.TaskExecutors;
 import io.micronaut.scheduling.annotation.ExecuteOn;
+import jakarta.validation.Valid;
 
 import java.time.LocalDate;
 import java.util.List;
@@ -30,6 +33,27 @@ public class ZutatDerWocheController {
     @Get(value = "/current")
     Optional<ZutatDerWoche> getCurrentZDW() {
         return zutatDerWocheRepository.findByVonLessThanEqualsAndBisGreaterThanEquals(LocalDate.now(), LocalDate.now());
+    }
+
+    @Post
+    @ExecuteOn(TaskExecutors.BLOCKING)
+    HttpResponse<ZutatDerWoche> createZutatDerWoche(@Body @Valid ZutatDerWoche zutat) {
+        var createdZDW = zutatDerWocheRepository.save(zutat);
+        return HttpResponse.status(HttpStatus.CREATED).body(createdZDW);
+    }
+
+    @Put
+    @ExecuteOn(TaskExecutors.BLOCKING)
+    HttpResponse<ZutatDerWoche> updateZutatDerWoche(@Body @Valid ZutatDerWoche zutat) {
+        var updatedZDW = zutatDerWocheRepository.update(zutat);
+        return HttpResponse.status(HttpStatus.OK).body(updatedZDW);
+    }
+
+    @Delete("/{id}")
+    @ExecuteOn(TaskExecutors.BLOCKING)
+    @Status(HttpStatus.NO_CONTENT)
+    void deleteZutatDerWoche(Long id) {
+        zutatDerWocheRepository.deleteById(id);
     }
 
 }
